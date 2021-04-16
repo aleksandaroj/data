ac048f8d412a630d77a2460ff4dfcbca923b80df
aleksandaroj <aleksandaroj@users.noreply.github.com>
2021-04-16 18:29:27 +0200

    Revert "Delete wip directory"
    This reverts commit 770afaf47b1d4fd9969218eb3179a7631fc94ded.

---
+++ wip/korisne python stvari.py
@@ -0,0 +1,28 @@
+c.execute("CREATE TABLE IF NOT EXISTS sales ({})".format(' ,'.join(df.columns))
+
+for row in df.iterrows():
+    sql = 'INSERT INTO salesdata ({}) VALUES ({})'.format(' ,'.join(df.columns), ','.join([?]*len(df.columns)))
+    c.execute(sql, tuple(row[1]))
+conn.commit()
+
+################
+
+try:
+    # create connection
+except Exception as err:
+    # print error
+else:
+    # print(conn.version)
+    try:
+        cur = conn.cursor()
+        sql_insert = """ """
+        cur.execute(sql_insert)
+    except Exception as err
+        print('Error while inserting', err)
+    else:
+        print('Insert done')
+        conn.commit()
+finally:
+    cur.close()
+    conn.close()
+    
---
+++ wip/lista-dict-lista.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 11 12:50:26 2021
+
+@author: Aleksandar
+"""
+
+l = [
+     {
+      "ime": "alek 1",
+      "prezime": "d 1",
+      "god": 1,
+      },
+     {
+      "ime": "alek 2",
+      "prezime": "d 2",
+      "god": 2,
+      },
+     {
+      "ime": "alek 3",
+      "prezime": "d 3",
+      "god": 1977,
+      },
+     ]
+
+d = []
+
+for index, x in enumerate(l):
+    
+    d.append(x["god"])
+print(type(d))
+print("Minimum i maksimum godina je: ", min(d), " i ", max(d))
+    
+
